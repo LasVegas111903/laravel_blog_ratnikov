1561f529795b5482cc2681dc8a2d87f73fe0305d
student5 <studkab5@jdk.ru>
2026-04-29 09:33:33 +0300

    4. связи в моделях

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
     use HasFactory;
+
+    protected $fillable = ['title', 'slug'];
+    
+
+    public function tags()
+    {
+        return $this->belongToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
--- app/Models/category.php
+++ app/Models/category.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class category extends Model
 {
+    use Sluggable;
     use HasFactory;
+
+    protected $fillable = ['title', 'slug'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
